962124bceeeb8220d230612861e3991d1634e729
CalvinNunes13 <35965305+CalvinNunes13@users.noreply.github.com>
2018-02-15 21:02:27 -0200

    unity files

---
+++ HavanaRPGUnity/Assets/BattleLib.cs
@@ -0,0 +1,297 @@
+﻿using HavanaRPG.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//
+
+namespace HavanaRPG.Model
+{  
+    class BattleLib
+    {
+        public static Player TempPlayer = new Player("", HavanaLib.ClassNames.Warrior, HavanaLib.Gender.None); //usado durante uma batalha
+        public static Player BeforeBattlePlayer = new Player("", HavanaLib.ClassNames.Warrior, HavanaLib.Gender.None); //salva o estado atual do player antes da batalha, caso precise voltar os dados depois
+
+        public static bool CheckIsDeath(decimal hp)
+        {
+            if (hp <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static decimal DoAttack(decimal atkPts, decimal def, decimal hp, HavanaLib.AdvDvd enemyAdvDvd)
+        {
+            decimal finalHp = 0;
+            decimal totalAtkPts = 0;
+            try
+            {
+                totalAtkPts = atkPts;
+                finalHp = hp;
+                if (enemyAdvDvd == HavanaLib.AdvDvd.Resistance)
+                {
+                    totalAtkPts = Math.Floor(atkPts + (atkPts * (decimal)0.5));
+                }
+                else if (enemyAdvDvd == HavanaLib.AdvDvd.Advantage)
+                {
+                    totalAtkPts = Math.Floor(atkPts + (atkPts * (decimal)0.25));
+                }
+                else if (enemyAdvDvd == HavanaLib.AdvDvd.Disadvantage)
+                {
+                    totalAtkPts = Math.Floor(atkPts - (atkPts * (decimal)0.25));
+                }
+                else if (enemyAdvDvd == HavanaLib.AdvDvd.Weakness)
+                {
+                    totalAtkPts = Math.Floor(atkPts - (atkPts * (decimal)0.5));
+                }
+                else
+                {
+                    totalAtkPts = atkPts;
+                }
+
+                if (totalAtkPts > def)
+                {
+                    finalHp = finalHp - (totalAtkPts - def);
+                    if (finalHp < 0)
+                    {
+                        finalHp = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                HavanaLib.MsgBox("Error applying function: 'DoAttack' \n" + ex.Message);
+                return hp;
+            }
+            return finalHp;
+        }
+        
+        //Temp Player//
+        public static void IncreasePlayerStatTemp(decimal value, HavanaLib.StatsNames stat)
+        {
+            switch (stat)
+            {
+                case HavanaLib.StatsNames.Strenght:
+                    TempPlayer.Strenght += value;
+                    break;
+
+                case HavanaLib.StatsNames.Magic:
+                    TempPlayer.Magic += value;
+                    break;
+
+                case HavanaLib.StatsNames.Dexterity:
+                    TempPlayer.Dexterity += value;
+                    break;
+
+                case HavanaLib.StatsNames.Creativity:
+                    TempPlayer.Creativity += value;
+                    break;
+
+                case HavanaLib.StatsNames.Winsdom:
+                    TempPlayer.Winsdom += value;
+                    break;
+            }
+        }
+
+        public static void TempPlayerLoseHP(decimal lostValue)
+        {
+            var hp = TempPlayer.HealthPts;
+            hp = hp - lostValue;
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+            TempPlayer.HealthPts = hp;
+        }
+
+        public static void TempPlayerGainHp(decimal value)
+        {
+            var hp = TempPlayer.HealthPts;
+            hp = hp + value;
+            if (hp > TempPlayer.MaxHealthPts)
+            {
+                hp = TempPlayer.MaxHealthPts;
+            }
+            TempPlayer.HealthPts = hp;
+        }
+
+
+        public static void TempPlayerLoseEnergy(decimal energyValue)
+        {
+            var ep = TempPlayer.EnergyPts;
+            ep = ep - energyValue;
+            if (ep < 0)
+            {
+                ep = 0;
+            }
+            TempPlayer.EnergyPts = ep;
+        }
+
+        public static void TempPlayerGainEnergy(decimal value)
+        {
+            var ep = TempPlayer.EnergyPts;
+            ep = ep + value;
+            if (ep > TempPlayer.MaxEnergyPts)
+            {
+                ep = TempPlayer.MaxEnergyPts;
+            }
+            TempPlayer.EnergyPts = ep;
+        }
+
+
+        //Real Player//
+        public static void IncreasePlayerStatReal(decimal value, HavanaLib.StatsNames stat)
+        {
+            switch (stat)
+            {
+                case HavanaLib.StatsNames.Strenght:
+                    GameController.GamePlayer.Strenght += value;
+                    break;
+
+                case HavanaLib.StatsNames.Magic:
+                    GameController.GamePlayer.Magic += value;
+                    break;
+
+                case HavanaLib.StatsNames.Dexterity:
+                    GameController.GamePlayer.Dexterity += value;
+                    break;
+
+                case HavanaLib.StatsNames.Creativity:
+                    GameController.GamePlayer.Creativity += value;
+                    break;
+
+                case HavanaLib.StatsNames.Winsdom:
+                    GameController.GamePlayer.Winsdom += value;
+                    break;
+            }
+        }
+
+        public static void RealPlayerLoseHP(decimal lostValue)
+        {
+            var hp = GameController.GamePlayer.HealthPts;
+            hp = hp - lostValue;
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+            GameController.GamePlayer.HealthPts = hp;
+        }
+
+        public static void RealPlayerGainHp(decimal value)
+        {
+            var hp = GameController.GamePlayer.HealthPts;
+            hp = hp + value;
+            if (hp > GameController.GamePlayer.MaxHealthPts)
+            {
+                hp = GameController.GamePlayer.MaxHealthPts;
+            }
+            GameController.GamePlayer.HealthPts = hp;
+        }
+
+
+        public static void RealPlayerLoseEnergy(decimal energyValue)
+        {
+            var ep = GameController.GamePlayer.EnergyPts;
+            ep = ep - energyValue;
+            if (ep < 0)
+            {
+                ep = 0;
+            }
+            GameController.GamePlayer.EnergyPts = ep;
+        }
+
+        public static void RealPlayerGainEnergy(decimal value)
+        {
+            var ep = GameController.GamePlayer.EnergyPts;
+            ep = ep + value;
+            if (ep > GameController.GamePlayer.MaxEnergyPts)
+            {
+                ep = GameController.GamePlayer.MaxEnergyPts;
+            }
+            GameController.GamePlayer.EnergyPts = ep;
+        }
+
+        public static void UseAbility(Ability ability)
+        {
+            UseAbility(ability, null, false);
+        }
+
+        public static void UseAbility(Ability ability, Creature enemy)
+        {
+            UseAbility(ability, enemy, false);
+        }
+
+        public static void UseAbility(Ability ability, Creature enemy, bool playerTarget)
+        {
+            //dynamic target;
+            //if (playerTarget)
+            //{
+            //    target = GameController.GamePlayer;
+            //}
+            //else
+            //{
+            //    target = enemy;
+            //}
+
+            //if (ability.HasBasicEffect)
+            //{
+            //    ability.BasicEffect();                
+            //}
+
+            //if (ability.HasSpecialEffect)
+            //{
+            //    ability.SpecialEffect();
+            //}
+            
+            //if (ability.HasStatusModifier)
+            //{
+            //    ability.AddRemoveStatus();
+            //}
+        }
+
+        public static void UseWeapon(Item weapon, Creature target, bool playerTarget)
+        {
+            if (weapon.HasSpecialEffect)
+            {
+                weapon.OnSpecialEffect();
+            }
+
+            if (weapon.DiceRolls > 0 && weapon.DiceSides > 0)
+            {
+
+            }
+        }
+
+        public static void RealPlayerUseDefend()
+        {
+            decimal defPts = GameController.GamePlayer.DefensePts;
+            defPts = defPts + (defPts * (decimal)0.5);
+            GameController.GamePlayer.CurrentDefPts = defPts;
+        }
+
+        public static void OnBattleStart()
+        {
+            BeforeBattlePlayer = GameController.GamePlayer;
+            TempPlayer = GameController.GamePlayer;
+        }
+
+        public static void OnBattleEnd()
+        {
+            GameController.GamePlayer.HealthPts = TempPlayer.HealthPts;
+            GameController.GamePlayer.EnergyPts = TempPlayer.EnergyPts;
+        }
+
+        public static void RestoreAllStats()
+        {
+            GameController.GamePlayer.Strenght = GameController.GamePlayer.MaxStrenght;
+            GameController.GamePlayer.Dexterity = GameController.GamePlayer.MaxDexterity;
+            GameController.GamePlayer.Magic = GameController.GamePlayer.MaxMagic;
+            GameController.GamePlayer.Creativity = GameController.GamePlayer.MaxCreativity;
+            GameController.GamePlayer.Winsdom = GameController.GamePlayer.MaxWinsdom;
+            GameController.GamePlayer.StatusEffects.Clear();
+            GameController.GamePlayer.CurrentDefPts = GameController.GamePlayer.DefensePts;
+        }
+
+    }
+}
---
+++ HavanaRPGUnity/Assets/HavanaLib.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//
+//using System.Windows.Forms;
+using System.Threading;
+using HavanaRPG.Controller;
+using UnityEngine;
+using UnityEditor;
+using System.Globalization;
+
+namespace HavanaRPG.Model
+{
+    public class HavanaLib
+    {
+        public enum raceNames { Human, Orc, Elf, Naga, Dwarf };
+        public enum ClassNames { None, Warrior, Sorcerer, Druid, Paladin, Archer, Monk, Thief };
+        public enum CitiesNames { Leto, Monxevis };
+        public enum Locations { Limbo, Leto, Monxevis, Gibraltar_Forest, };
+        public enum StatsNames { Strenght, Magic, Dexterity, Creativity, Winsdom }
+        public enum StatusNames { None, Blind, Slow, Confused };
+        public enum DayTime { Morning, Afternoon, Night };
+        public enum AdvDvd { None, Resistance, Advantage, Normal, Disadvantage, Weakness }
+        public enum Elements { None, Water, Fire, Air, Earth, Dark, Light, Physical }
+        public enum Gender { None, Male, Female };
+        public enum ItemTypes { None, Weapons, Armors, Generic, Potions, Magical, Archery }
+        public enum PastHistories { None }
+
+        public HavanaLib() { }
+
+        public static void NewGame()
+        {
+        }
+
+        public static void SaveGame()
+        {
+            try
+            {
+                GameController.SavePlayerInfo();
+                GameController.SaveGameState();
+            }
+            catch (Exception ex)
+            {
+                MsgBox("ERROR WHILE SAVING! The game may not be correctly saved\n" + ex.Message + "\n Contact the developers at HavanaRpg@outlook.com", "error", "ERROR");
+            }
+        }
+
+        public static void LoadGame()
+        {
+            try
+            {
+                GameController.LoadPlayerInfo();
+                GameController.LoadGameState();
+            }
+            catch (Exception ex)
+            {
+                MsgBox("ERROR WHILE LOADING! The game will not start.\n" + ex.Message + "\n Contact the developers at HavanaRpg@outlook.com", "error", "ERROR");
+            }
+
+        }
+
+        public static ClassNames ReturnEnumClassByString(string value)
+        {
+            var enumOption = (ClassNames)Enum.Parse(typeof(ClassNames), value);
+            return enumOption;
+        }
+
+        public static Gender ReturnEnumGenderByString(string value)
+        {
+            var enumOption = (Gender)Enum.Parse(typeof(Gender), value);
+            return enumOption;
+        }
+
+        public static string ToProperCase(string text)
+        {
+            text = text.ToLower();
+            var properCulture = new CultureInfo("en-US", false).TextInfo;
+            var textReturn = properCulture.ToTitleCase(text);
+            return textReturn;
+        }
+
+        //Retorna a quantidade de lançamentos de um dado
+        public static decimal ReturnDiceRolls(string diceNameStyle) //entrada exemplo: 2d8, retorno 2
+        {
+            decimal diceRolls = 1;
+            diceNameStyle = diceNameStyle.ToLower();
+            diceNameStyle = diceNameStyle.Replace(" ", "");
+            try
+            {
+                var splitReturn = diceNameStyle.Split('d');
+                diceRolls = int.Parse(splitReturn[0]);
+            }
+            catch (Exception ex)
+            {
+                return 1;
+            }
+            return diceRolls;
+        }
+
+        //Retorna a quantidade de lados de um dado
+        public static decimal ReturnDiceSides(string diceNameStyle) //entrada exemplo: 3d6, retorno 6
+        {
+            decimal diceSides = 1;
+            diceNameStyle = diceNameStyle.ToLower();
+            diceNameStyle = diceNameStyle.Replace(" ", "");
+            try
+            {
+                var splitReturn = diceNameStyle.Split('d');
+                diceSides = int.Parse(splitReturn[1]);
+            }
+            catch (Exception ex)
+            {
+                return 1;
+            }
+            return diceSides;
+        }
+
+        //Chama uma função a cada certo período de tempo
+//        public static async Task RunLoopTimer(Action action, TimeSpan timeout)
+//        {
+//            await Task.Delay(timeout).ConfigureAwait(false);
+//
+//            action();
+//
+//            RunLoopTimer(action, timeout);
+//
+//        }
+        //Verifica se objeto é vazio
+        public static Boolean IsEmpty(object obj)
+        {
+            if (obj == null)
+                return true;
+            else
+            {
+                if (obj.ToString().Trim().Length == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //quando acaba o jogo
+        public static void GameOver()
+        {
+
+        }
+
+        //Exibe imagem do dado na tela com o valor
+        public static void ShowDiceOnScreen(decimal diceValue)
+        {
+
+        }
+
+        //verifica randomEvents
+        public static void VerifyRandomEvent(List<RandomEvent> randomEvents)
+        {
+            VerifyRandomEvent(randomEvents, 1);
+        }
+
+        public static void VerifyRandomEvent(List<RandomEvent> randomEvents, int numberOfEvents)
+        {
+            var eventsHappen = 0;
+            foreach (var rndEvent in randomEvents)
+            {
+                var prob = rndEvent.Probability;
+                if (prob > 100)
+                {
+                    prob = 100;
+                }
+                if (prob > 0)
+                {
+                    var testFor = new System.Random();
+                    var returnedRnd = testFor.Next(0, 100);
+                    if (returnedRnd <= prob)
+                    {
+                        rndEvent.OnEventCall();
+                        eventsHappen++;
+                    }
+                }
+                if (eventsHappen >= numberOfEvents)
+                {
+                    break;
+                }
+            }
+        }
+
+        //Chama na view de talk uma pergunta com respostas
+        public static void CreateQuestionTalks(string question, List<string> answers)
+        {
+            if (ViewsController.CurrentViewName.ToUpper() == "TALKVIEW")
+            {
+
+            }
+        }
+
+        //Exibe pop-up de mensagem
+        public static void MsgBox(string msg)
+        {
+            MsgBox(msg, "info", "");
+        }
+
+        public static void MsgBox(string msg, string type)
+        {
+            MsgBox(msg, type, "");
+        }
+
+        public static void MsgBox(string msg, string type, string caption) //alert, error, info
+        {
+            if (type.ToUpper() == "ALERT")
+            {
+                EditorUtility.DisplayDialog(caption, msg , "Ok");
+            }
+            else if (type.ToUpper() == "ERROR")
+            {
+                EditorUtility.DisplayDialog(caption, msg, "Ok");
+            }
+            else
+            {
+                EditorUtility.DisplayDialog(caption, msg, "Ok");
+            }
+        }
+
+        //exibe pop-up de mensagem com botões de confirmar e cancelar
+        public static bool ConfirmBox(string msg) //alert, error, info
+        {
+            var ret = EditorUtility.DisplayDialog("?", msg, "Ok", "Cancel");
+            
+            if (ret)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
